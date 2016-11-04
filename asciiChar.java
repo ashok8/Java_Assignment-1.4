9af3bcce9ecf5b3b78f1b2d1f0324e4091b62d79
ashok8 <ashok8@users.noreply.github.com>
2016-11-04 19:37:26 +0530

    Add files via upload

---
+++ asciiChar.java
@@ -0,0 +1,10 @@
+public class asciiChar {
+
+	public static void main(String[] args) {
+		for(int num=65;num<=97;num++){
+			char c = (char)num;
+			System.out.printf("%d - %s\n", num, c);
+		}	
+	}
+
+}
